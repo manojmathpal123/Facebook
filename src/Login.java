aeeec99e96f4cdf7d4b9ec3ef73316f4c88cfee3
John Doe <john@sample.com>
2017-11-14 14:01:46 +0530

    changes in new code for poc

---
+++ src/Login.java
@@ -0,0 +1,61 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import testlink.api.java.client.TestLinkAPIClient;
+import testlink.api.java.client.TestLinkAPIException;
+import testlink.api.java.client.TestLinkAPIResults;
+
+public class Login {
+
+	public static WebDriver dr;
+	public static String DEV_KEY = "441b106fbc9ec8537fa53f4a64ae4a7d";
+
+	public static String SERVER_URL = "http://localhost/testlink-1.9.16/lib/api/xmlrpc/v1/xmlrpc.php";
+	public static String PROJECT_NAME = "testlinkproject_poc";
+	public static String PLAN_NAME = "testplan";
+	public static String BUILD_NAME = "sample-build";
+	public static String TESTCASE   ="ID-1";
+	
+	// private String exception;
+	String exception = null;
+
+	@Test
+
+	public void logindata() throws Exception {
+
+		String result = "";
+		String exception = null;
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\manoj.m\\Downloads\\colt\\Colt_Project\\drivers\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		try {
+			driver.get("https://accounts.google.com");
+			driver.manage().window().maximize();
+		/*	result = TestLinkAPIResults.TEST_PASSED;
+		    driver.findElement(By.id("Email")).sendKeys("your gmail login id");
+			Thread.sleep(2000);
+			driver.findElement(By.id("next")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.id("Passwd")).sendKeys("*********");
+			Thread.sleep(1000);
+			driver.findElement(By.id("signIn")).click();*/
+			result = TestLinkAPIResults.TEST_PASSED;
+			updateTestLinkResult(TESTCASE, null, result);
+		} 
+		catch (Exception e) {
+			result = TestLinkAPIResults.TEST_FAILED;
+			exception = e.getMessage();
+			updateTestLinkResult(TESTCASE, exception, result);
+		}
+
+	}
+
+	private void updateTestLinkResult(String testCase, String exception, String result) throws TestLinkAPIException {
+		TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(DEV_KEY, SERVER_URL);
+		testlinkAPIClient.reportTestCaseResult(PROJECT_NAME, PLAN_NAME, TESTCASE, BUILD_NAME, exception, result);
+
+	}
+
+}
