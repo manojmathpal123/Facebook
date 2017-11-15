4762063c93d7e092e71c100768a799a04335769f
John Doe <john@sample.com>
2017-11-15 12:44:15 +0530

    exe path added

--- src/Login.java
+++ src/Login.java
@@ -27,7 +27,7 @@ public class Login {
 		String result = "";
 		String exception = null;
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\manoj.m\\Downloads\\colt\\Colt_Project\\drivers\\chromedriver.exe");
+				"C:\\Program Files (x86)\\Jenkins\\workspace\\testlink_poc\\test-output\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		try {
